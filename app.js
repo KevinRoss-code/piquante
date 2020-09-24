ce41ea93b440e5494ba1be442750bf985de8b3b5
kevin <kevin.rossignol37@yahoo.com>
2020-09-24 11:49:16 +0200

    début

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const userRouteur = require('./routes/user');
+
 
 const app = express();
 
-//mise ne place des middleware qui sont des variables et permettent d'éxecuter le code
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -11,38 +13,64 @@ app.use((req, res, next) => {
     next();
 });
 
+const uri = "mongodb+srv://loursenrage:<Asc1THn9>@bdd-piquante.m0qvo.mongodb.net/<BDD-piquante>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+//mise ne place des middleware qui sont des variables et permettent d'éxecuter le code
+
+
 app.use(bodyParser.json());
 
-app.post('/api/auth/signup', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Creer'
+app.use('/api/auth', userRouteur);
+
+module.exports = app;
+
+
+
+/*app.post('/api/auth/signup', (req, res, next) => {
+    delete req.body._id;
+    const sign = new Sign({
+        ...req.body
     });
+    sign.save()
+    .the(() => res.status(201).json({ message: 'Inscription réussite'}))
+    .catch(error => res.status(400).json({ error }));
     next();
 });
 
-app.post('/api/auth/login', (req, res, next) => {
-    console.log(req.body);
-    res.status(202).json({
-        userId:'string',
-        token:'string'
-    });
-    next();
-});
 
 app.post('/api/sauces', (req, res, next) =>{
-    console.log(req.body);
-    res.status(202).json({
-        sauce:'chaine',
-        image:'fichier',
+    delete req.body._id;
+    const sauces = new Sauces({
+        ...req.body
     });
+    sauces.save()
+    .the(() => res.status(201).json({ message: 'Sauce enregistrée'}))
+    .catch(error => res.status(400).json({ error }));
+    next();
     next();
 });
 
+app.use('/signup', (req, res, next) => {
+    const newUser = [
+        {
+            userId: 'string',
+            email: 'string',
+            password: 'string'
+        }
+    ];
+    next()
+;})
 app.use('/api/sauces', (req, res, next) => {
     const sauce = [
         {
-            id: 'ObjectID',
+            _id: 'ObjectID',
             userId: 'string',
             name: 'string',
             manufacteur: 'string',
@@ -59,9 +87,4 @@ app.use('/api/sauces', (req, res, next) => {
     res.status(200).json(sauce);
     next();
 });
-
-app.use((req, res, next) => {
-    console.log('reçu')
-})
-
-module.exports = app;
+*/
